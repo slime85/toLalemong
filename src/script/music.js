e230ecfcc15712d213acbdd31946c010a1e74253
MR_Slime <91015619+slime85@users.noreply.github.com>
2022-07-19 02:17:13 +0900

    Add files via upload

--- src/script/music.js
+++ src/script/music.js
@@ -79,7 +79,7 @@ const musicAnimation = () => {
       }
     }
     for (let i = 0; i < selMusic.bufferLength; i++) {
-      barList.push(barLists[i % 3].splice(0, 1));
+      barList.push(barLists[i % 3].splice(0, 1)[0]);
     }
     avr /= selMusic.bufferLength;
     if(avr > 7) {
@@ -124,7 +124,7 @@ const musicAnimation = () => {
     drwCtx.fill();
     drwCtx.globalCompositeOperation = "source-over";
 
-    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 5 * 2.5);
+    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 5 * 2.7);
   }
     
   for(let i = 0; i < musics.length; i++) {
@@ -176,7 +176,7 @@ const musicAnimation = () => {
 
     musicCanvas.rangeButton = drwCtx.lineWidth * 2.3;
     
-    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 5 * 2.5);
+    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 5 * 2.7);
   }
 
   for(let i = 0; i < musics.length; i++) {
@@ -215,7 +215,7 @@ const musicAnimation = () => {
 
     musicCanvas.playButton = $drawCanvas.width / 9;
 
-    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 10 * 7.5);
+    drawImage(mpCtx, $drawCanvas, (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x, page.h / 10 * 7.7);
   }
   
   requestAnimationFrame(musicAnimation);
@@ -262,7 +262,7 @@ document.addEventListener("mousedown", e => {
     for(let i = 0; i < musicList.length; i++) {
       const music = musicList[i].music;
       let dx = (page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x;
-      let dy = page.h / 5 * 2.5;
+      let dy = page.h / 5 * 2.7;
       let time = music.currentTime / music.duration;
       
       let tx = dx + Math.cos(pi * 1.5 * (time)) * (musicCanvas.albumSize * 1.12) / 2;
@@ -291,7 +291,7 @@ document.addEventListener("mousemove", e => {
     if(typeof(mouse.md) == "object") {
       const music = musicList[mouse.md.idx].music;
       let dx = (page.w / 10) + ((page.w - page.w / 5) / 3 * (mouse.md.idx + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x;
-      let dy = page.h / 5 * 2;
+      let dy = page.h / 5 * 2.7;
       let theta = Math.atan2(mouse.y - dy, mouse.x - dx);
       
       if(mouse.md.type == "time") {
@@ -336,7 +336,7 @@ document.addEventListener("mouseup", e => {
     if(mouse.dx == mouse.x && mouse.dy == mouse.y) {
       console.log("click");
       for(let i = 0; i < musicList.length; i++) {
-        let r = Math.sqrt(Math.pow(mouse.x - ((page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x), 2) + Math.pow(mouse.y - (page.h / 10 * 7.5), 2));
+        let r = Math.sqrt(Math.pow(mouse.x - ((page.w / 10) + ((page.w - page.w / 5) / 3 * (i + 1)) - ((page.w - page.w / 5) / 3 / 2) - musicCanvas.x), 2) + Math.pow(mouse.y - (page.h / 10 * 7.7), 2));
 
         if(r < musicCanvas.playButton) {
           if(musicList[i].music.paused) {
