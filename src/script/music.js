650f107244a6ece2955e162e402450fc99eda2b9
MR_Slime <91015619+slime85@users.noreply.github.com>
2022-07-21 02:28:39 +0900

    Add files via upload

--- src/script/music.js
+++ src/script/music.js
@@ -85,7 +85,7 @@ const musicAnimation = () => {
   }else {
     barList = prevBarList;
     
-    for(let i = 0; i < barList.length; i++) barList[i] -= 0.5;
+    for(let i = 0; i < barList.length; i++) barList[i] -= 1;
   }
 
   prevBarList = barList;
